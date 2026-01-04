83e9c2ca01383b9e15f3722aaf768d6be1758cf1
Anders Aa. Hagen <aaahagen@gmail.com>
2026-01-04 09:19:11 +0000

    why is'nt the page loading

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,12 +4,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyD1j9gNDdFUa-a4tg4EMGu66TOXDDynrBo',
-  authDomain: 'studio-5726715282-ae599.firebaseapp.com',
-  projectId: 'studio-5726715282-ae599',
-  storageBucket: 'studio-5726715282-ae599.appspot.com',
-  messagingSenderId: '273761981942',
-  appId: '1:273761981942:web:bd7ede059c1e4a953be3d7',
+  apiKey: "AIzaSyD1j9gNDdFUa-a4tg4EMGu66TOXDDynrBo",
+  authDomain: "studio-5726715282-ae599.firebaseapp.com",
+  projectId: "studio-5726715282-ae599",
+  storageBucket: "studio-5726715282-ae599.appspot.com",
+  messagingSenderId: "273761981942",
+  appId: "1:273761981942:web:bd7ede059c1e4a953be3d7",
 };
 
 // Initialize Firebase
