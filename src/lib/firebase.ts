4f6f0ebb54c025c5c3fa6d398855c983745cfe14
Anders Aa. Hagen <aaahagen@gmail.com>
2026-01-04 19:23:15 +0000

    dette tok tid?

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,13 +4,14 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "YOUR_API_KEY",
+  authDomain: "studio-5726715282-ae599.firebaseapp.com",
+  projectId: "studio-5726715282-ae599",
+  storageBucket: "studio-5726715282-ae599.appspot.com",
+  messagingSenderId: "5726715282",
+  appId: "1:5726715282:web:35c8e3123845c43232938f"
 };
+
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
